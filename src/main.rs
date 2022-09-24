c7dd819bc06d1cf99017d1ab95a8b51b3a51d1f8
khadievedem <khadiev.edem@gmail.com>
2022-09-24 07:21:14 +0300

    add random for different ABI

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,28 @@ use core::arch::global_asm;
 
 global_asm!(include_str!("random.asm"));
 
+// extern "efiapi" {
+//     fn efi_rand(seed: i64) -> i64;
+// }
+// extern "win64" {
+//     fn win64_rand(seed: i64) -> i64;
+// }
+
 extern "sysv64" {
-    fn random() -> i64;
+    fn system_rand(seed: i64) -> i64;
 }
 
+fn asm_random() -> u32 {
+    let seed = rand::random::<i64>();
+    let prime: u32 = 289035269;
+    let res;
+
+    unsafe {
+        res = system_rand(seed);
+    }
+    res as u32 % prime
+}
 
 fn main() {
-    println!("{}", unsafe{random()});
+    println!("{}", asm_random());
 }
--- src/random.asm
+++ src/random.asm
@@ -1,9 +1,36 @@
 .section .text
 .code64
-.global random
 
-random:
-    xor rax, rax
-    rdtsc
+pe_rand:
+    push rbx 
+    push rdx
+    rdtsc                    
+    mov rbx, rax            
+    mov rax, rcx            
+    div rbx
+    mul rdx
+    pop rdx
+    pop rbx
     ret
 
+efi_rand:
+    call pe_rand
+
+win64_rand:
+    call pe_rand
+
+system_rand:
+    push rbx 
+    push rdx
+    rdtsc                    
+    mov rbx, rax            
+    mov rax, rdi            
+    div rbx
+    mul rdx
+    pop rdx
+    pop rbx
+    ret
+
+.section .data
+rand_file:
+    .asciz "/dev/random"
