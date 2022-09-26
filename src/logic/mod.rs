10cc2864ffc5bdac8ed3527575b0c3c8d6e42c6e
khadievedem <khadiev.edem@gmail.com>
2022-09-26 21:54:38 +0300

    add general db management instructions

--- src/logic/mod.rs
+++ src/logic/mod.rs
@@ -1,25 +1,72 @@
-#[derive(Debug)]
+// mod.rs
+use core::arch::global_asm;
+use std::fs;
+use std::mem::drop;
 
-pub struct Student {
-    id: u8,
-    name: String,
-    surname: String,
-    patronymic: String,
+global_asm!(include_str!("random.asm"));
+
+extern "sysv64" {
+    fn system_rand(seed: i64, module: i64) -> i64;
 }
 
-pub struct Variant {
-    id: u8,
-    path: String,
-}
+// extern "efiapi" {
+//     fn efi_rand(seed: i64) -> i64;
+// }
+// extern "win64" {
+//     fn win64_rand(seed: i64) -> i64;
+// }
 
-impl Student {
-    pub fn new(id: u8, name: String, surname: String, patronymic: String) -> Student {
-        Student(id, name, surname, patronymic)
+pub mod randi64 {
+    fn asm_random(module: i64) -> u32 {
+        let seed = rand::random::<i64>();
+        let prime: u32 = 289035269;
+        let res;
+
+        unsafe {
+            res = system_rand(seed, module);
+        }
+        res as u32 % prime
     }
 }
 
-impl Variant {
-    pub fn new(id: u8, path: String) -> Student {
-        Variant(id, path)
+pub mod tablmgr {
+    use std::fs::write;
+    use std::fs::OpenOptions;
+    use std::path::Path;
+
+    fn add(_tbl: String, data: String) {
+        let output = Path::new(format!("../../static_data/{}.txt", _tbl).as_str());
+        let file = OpenOptions::new()
+            .append(true)
+            .open(output)
+            .expect("[ERROR] unable to open file");
+        let new_data = data.trim().replace(" ", ",");
+
+        write(output, new_data).expect("[ERROR] unable to wirte file");
+        drop(file);
+    }
+
+    fn edit(_tbl: String, id: u32) {
+        let output = Path::new(format!("../../static_data/{}.txt", _tbl).as_str());
+        let file = OpenOptions::new()
+            .write(true)
+            .open(output)
+            .expect("[ERROR] unable to open file");
+    }
+
+    fn del(_tbl: String, id: u64) -> String {
+        let output = Path::new(format!("../../static_data/{}.txt", _tbl).as_str());
+        let file = OpenOptions::new()
+            .write(true)
+            .open(output)
+            .expect("[ERROR] unable to open file");
+    }
+
+    fn print(_tbl: String, id: u32) -> String {
+        let output = Path::new(format!("../../static_data/{}.txt", _tbl).as_str());
+        let file = OpenOptions::new()
+            .read(true)
+            .open(output)
+            .expect("[ERROR] unable to open file");
     }
 }
