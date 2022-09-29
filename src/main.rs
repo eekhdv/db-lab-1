30a2d6ea6a98990627031aba59e278d73d3963ad
khadievedem <khadiev.edem@gmail.com>
2022-09-30 00:05:48 +0300

    base gui app

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 mod logic;
+use dioxus::prelude::*;
 
 fn main() {
+    dioxus::desktop::launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    cx.render(rsx!{
+        div {
+            "hello world!"
+        }
+    })
 }
