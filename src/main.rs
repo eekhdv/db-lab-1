d5cfafbae04107c0723017bdfdaefa582b464714
khadievedem <khadiev.edem@gmail.com>
2022-09-30 02:16:55 +0300

    remove dioxus

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
 mod logic;
-use dioxus::prelude::*;
 
-fn main() {
-    dioxus::desktop::launch(app);
-}
-
-fn app(cx: Scope) -> Element {
-    cx.render(rsx!{
-        div {
-            "hello world!"
-        }
-    })
-}
+fn main() {}
