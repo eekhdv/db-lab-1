4f3e78183b06cf85813db495a10b936c31489b44
khadievedem <khadiev.edem@gmail.com>
2022-10-01 18:48:42 +0300

    playing with gui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
-mod logic;
+use eframe::{
+    egui::{CentralPanel, Context, Grid},
+    epaint::Vec2,
+};
 
-fn main() {}
+fn main() {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Informational table",
+        options,
+        Box::new(|_cc| Box::new(MyApp)),
+    );
+}
+
+struct MyApp;
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello World!");
+            let test = Grid::spacing(Grid::new("general_table"), Vec2::new(20.0, 1.0));
+            test.show(ui, |ui| {
+                ui.label("First row, first column");
+                ui.label("First row, second column");
+                ui.end_row();
+
+                ui.label("Second row, first column");
+                ui.label("Second row, second column");
+                ui.label("Second row, third column");
+                ui.end_row();
+
+                ui.horizontal(|ui| {
+                    ui.label("Same");
+                    ui.label("cell");
+                });
+                ui.label("Third row, second column");
+                ui.end_row();
+            });
+        });
+        // egui::CentralPanel::default().show(ctx, |ui| {
+        //     ui.heading("My egui Application");
+        //     ui.horizontal(|ui| {
+        //         ui.label("Your name: ");
+        //         ui.text_edit_singleline(&mut self.name);
+        //     });
+        //     ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+        //     if ui.button("Click each year").clicked() {
+        //         self.age += 1;
+        //     }
+        //     ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        // });
+    }
+}
