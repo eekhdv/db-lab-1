fa2f83099fc87c2b6a79caa42b137a9fee795b78
khadievedem <khadiev.edem@gmail.com>
2022-10-03 07:41:09 +0300

    clean main

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ mod gui;
 mod logic;
 
 fn main() {
-    logic::tablmgr::create(".temp".to_string(), 'a');
-    logic::tablgen::gen_test_table();
-    let options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Informational table",
-        options,
-        Box::new(|_cc| Box::new(gui::tablgui::MyApp)),
-    );
-    match logic::tablmgr::clean() {
-        Ok(()) => eprintln!("[INFO] Generated tables clean successfully!"),
-        Err(_) => eprintln!("[ERROR] Can't clean generated tables"),
-    };
-    return;
+    // logic::tablmgr::create(".temp".to_string(), 'a');
+    // logic::tablgen::gen_test_table();
+    // let options = eframe::NativeOptions::default();
+    // eframe::run_native(
+    //     "Informational table",
+    //     options,
+    //     Box::new(|_cc| Box::new(gui::tablgui::MyApp)),
+    // );
+    // match logic::tablmgr::clean() {
+    //     Ok(()) => eprintln!("[INFO] Generated tables clean successfully!"),
+    //     Err(_) => eprintln!("[ERROR] Can't clean generated tables"),
+    // };
+    // return;
 }
